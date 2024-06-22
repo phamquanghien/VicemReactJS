87ccad507c23cddb827c874a01a3385d8e0f073d
phamquanghien <pqh.cntt@gmail.com>
2024-06-22 14:10:03 +0700

    Account

--- src/components/account/LoginModal.js
+++ src/components/account/LoginModal.js
@@ -1,31 +1,30 @@
-// LoginModal.js
 import React, { useState } from 'react';
 import { Modal, Button, Form } from 'react-bootstrap';
 import axios from 'axios';
-
 function LoginModal({ show, handleClose, setIsLoggedIn }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [rememberMe, setRememberMe] = useState(false);
-
   const handleLogin = async () => {
     try {
       const response = await axios.post('http://localhost:5075/api/Account/login', {
         email,
         password,
         rememberMe
-      }, { withCredentials: true });
-
-      if (response.data) {
+      });
+      if (response.data && response.data.token) {
+        const { token } = response.data;
+        localStorage.setItem('token', token);
         setIsLoggedIn(true);
         handleClose();
+      } else {
+        throw new Error('Invalid response data');
       }
     } catch (error) {
       console.error('Error logging in', error);
       alert('Login failed. Please check your credentials.');
     }
   };
-
   return (
     <Modal show={show} onHide={handleClose}>
       <Modal.Header closeButton>
@@ -35,43 +34,23 @@ function LoginModal({ show, handleClose, setIsLoggedIn }) {
         <Form>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="Enter email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
+            <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
           </Form.Group>
-
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+            <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
           </Form.Group>
-
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check
-              type="checkbox"
-              label="Remember Me?"
-              checked={rememberMe}
-              onChange={(e) => setRememberMe(e.target.checked)}
-            />
+            <Form.Check type="checkbox" label="Remember Me?" checked={rememberMe} onChange={(e) => setRememberMe(e.target.checked)} />
           </Form.Group>
         </Form>
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
-          Close
-        </Button>
-        <Button variant="primary" onClick={handleLogin}>
-          Login
-        </Button>
+        <Button variant="secondary" onClick={handleClose}>Close</Button>
+        <Button variant="primary" onClick={handleLogin}>Login</Button>
       </Modal.Footer>
     </Modal>
   );
 }
+
 export default LoginModal;
--- src/components/home/Menu.js
+++ src/components/home/Menu.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-import Navbar from 'react-bootstrap/Navbar';
+import React, {useState, useEffect} from 'react';
+import { Container, Navbar, Nav, Button } from 'react-bootstrap';
+import LoginModal from '../account/LoginModal';
 
 const Menu = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showLoginModal, setShowLoginModal] = useState(false);
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    setIsLoggedIn(!!token);
+  }, []);
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setIsLoggedIn(false);
+  };
+  const handleLoginClick = () => {
+    setShowLoginModal(true);
+  };
+  const handleCloseLoginModal = () => {
+    setShowLoginModal(false);
+  };
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="/">Home</Navbar.Brand>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="/">Home</Navbar.Brand>
+          <Nav className="ml-auto">
+              {isLoggedIn ? (
+                <Nav.Link href="#" onClick={handleLogout}>Logout</Nav.Link>
+              ) : (
+                <Button onClick={handleLoginClick}>Login</Button>
+              )}
+            </Nav>
+
+        </Container>
+      </Navbar>
+      <LoginModal show={showLoginModal} handleClose={handleCloseLoginModal} setIsLoggedIn={setIsLoggedIn} />
+    </>
   );
 };
 export default Menu;
